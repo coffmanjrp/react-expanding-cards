a47e6d4cfc0d47a29df6c081658c9933ff8a3dbb
coffmanjrp <coffmanjrp@gmail.com>
2021-08-17 20:58:19 -0700

    Implement expanding function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
+import data from './data/data';
 import './App.scss';
 
 function App() {
+  const [panels, setPanels] = useState(data);
+
+  const handleClick = (id) => {
+    const newPanels = panels.map((panel) =>
+      panel.id === id
+        ? { ...panel, active: !panel.active }
+        : { ...panel, active: false }
+    );
+
+    setPanels(newPanels);
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {panels.length > 0 &&
+        panels.map((panel) => (
+          <div
+            key={panel.id}
+            className={`panel${panel.active ? ' active' : ''}`}
+            style={{ backgroundImage: `url('${panel.url}')` }}
+            onClick={() => handleClick(panel.id)}
+          >
+            <h3>{panel.text}</h3>
+          </div>
+        ))}
     </div>
   );
 }
